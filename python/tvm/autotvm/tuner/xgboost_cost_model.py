881673e6bedb85a0099b045eb8c582a1292c6a68
Andrew Liu <andrewlliu@gmail.com>
2021-08-31 21:49:27 -0700

    Pytorch and x86 Benchmark Script (#18)

--- python/tvm/autotvm/tuner/xgboost_cost_model.py
+++ python/tvm/autotvm/tuner/xgboost_cost_model.py
@@ -422,7 +422,10 @@ def custom_callback(stopping_rounds, metric, fevals, evals=(), log_file=None,
     # pylint: disable=import-outside-toplevel
     from xgboost.core import EarlyStopException
     from xgboost.callback import _fmt_metric
-    from xgboost.training import aggcv
+    try:
+        from xgboost.training import aggcv
+    except ImportError:
+        from xgboost.callback import _aggcv as aggcv
 
     state = {}
     metric_shortname = metric.split("-")[1]
